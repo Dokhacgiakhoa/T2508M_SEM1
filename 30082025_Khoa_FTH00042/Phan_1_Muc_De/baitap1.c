57f1d451d1d0f69556f50102c873adb15db36812
Đỗ Khắc Gia Khoa <dokhacgiakhoa666@gmail.com>
2025-08-30 20:24:14 +0700

    update

---
+++ 30082025_Khoa_FTH00042/Phan_1_Muc_De/baitap1.c
@@ -0,0 +1,31 @@
+/*1. Giá trị tuyệt đối 
+Viết hàm tı́nh giá trị tuyệt đối của một số nguyên.	
+Prototype: int abs_i(int x); */
+#include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+int abs_i(int x) {
+    if (x < 0) {
+        return -x;
+    } else {
+        return x;
+    }
+}
+
+int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+    int x;
+    char c;
+    printf("Nhập vào số nguyên n: \n ");
+    while (scanf("%d%c", &x, &c) != 2 || c != '\n') {
+        printf("Giá trị bạn nhập vào không hợp lệ. Vui lồng nhập lại: ");
+        while (getchar() != '\n');
+    }
+    printf("Giá trị tuyệt đối của %d là %d", x, abs_i(x));
+    return 0;
+}
