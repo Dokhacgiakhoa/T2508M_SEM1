5277fabd39b503159f49432b92998fee3f624a82
Đỗ Khắc Gia Khoa <dokhacgiakhoa666@gmail.com>
2025-08-31 23:55:16 +0700

    update

---
+++ 30082025_Khoa_FTH00042/Phan_1_Muc_De/baitap6.c
@@ -0,0 +1,36 @@
+/* Lũy thừa 
+Tı́nh a^b với b >= 0.	
+Prototype: long long pow_int(int a, int b); */
+#include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+long long pow_int(int a, int b) {
+    long long result = 1;
+    for (int i = 0; i < b; i++) {
+        result *= a;
+    }
+    return result;
+}
+
+int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+    int a, b;
+    printf("Nhập vào số nguyên a: ");
+    while (scanf("%d", &a) != 1) {
+        printf("Gía trị nhập vào không hợp lệ, vui lòng nhập lại: ");
+        while (getchar() != '\n');
+    }
+    printf("Nhập vào số nguyên b: ");
+    while (scanf("%d", &b) != 1 || b < 0) {
+        printf("Giá trị nhập vào không hợp lệ, vui lòng nhập lại: ");
+        while (getchar() != '\n');
+    }
+    long long result = pow_int(a, b);
+    printf("%d^%d = %lld\n", a, b, result);
+    return 0;
+}
