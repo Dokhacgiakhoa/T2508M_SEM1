668eb396e5fa655ee93d32cadd78857b6924957b
Đỗ Khắc Gia Khoa <dokhacgiakhoa666@gmail.com>
2025-09-01 07:31:50 +0700

    update

---
+++ 30082025_Khoa_FTH00042/Phan_1_Muc_De/baitap7.c
@@ -0,0 +1,33 @@
+/* Số chữ số 
+Đếm số chữ số của một số nguyên.	
+Prototype: int digit_count(long long n); */
+
+#include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+int digit_count(long long n){
+    int count = 0;
+    while(n > 0){
+        n /= 10;
+        count++;
+    }
+    return count;
+}
+
+int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+    long long n;
+    printf("Nhập vào một số nguyên: ");
+    while (scanf("%lld", &n) != 1) {
+        printf("Giá trị nhập vào không hợp lệ, vui lòng nhập lại: ");
+        while (getchar() != '\n');
+    }
+    int count = digit_count(n);
+    printf("Số chữ số của %lld là: %d\n", n, count);
+    return 0;
+}
